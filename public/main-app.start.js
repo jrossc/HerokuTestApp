8b422396f78370a58bdba694a54368a9299c04e2
small4ward1 <32926946+small4ward1@users.noreply.github.com>
2017-11-11 21:22:56 +0800

    MVA Angular Tutorial deployed in Heroku

---
+++ public/main-app.start.js
@@ -0,0 +1,135 @@
+﻿"use strict";
+
+angular.module('MainApp', [
+	])
+
+.controller("MainController", function ($scope, $http) {
+	//initialize scope variables
+	$scope.people = [];
+	$scope._name = "Default Name";
+	$scope._location = "Default Location";
+	$scope.user = {
+		name: function (theName) {
+			if (angular.isDefined(theName)) {
+				$scope._name = theName;
+			}
+			return $scope._name;
+		}
+		(),
+		location: function (theLocation) {
+			if (angular.isDefined(theLocation)) {
+				$scope._location = theLocation;
+			}
+			return $scope._location;
+		}
+		()
+	};
+
+	//initialize config for headers
+	var config = {
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+			'ZUMO-API-VERSION': '2.0.0'
+		},
+	};
+
+	//call getNames function
+	getNames();
+
+	function getNames() {
+		$http.get('/api/users', config)
+		.then(function (res) {
+			console.log(res);
+			$scope.people = res.data;
+		});
+	}
+
+	// add in resource
+	function addName(user) {
+		var confirmres = confirm("about to post, Continue?");
+
+		if (confirmres == true) {
+			//check if user exists
+			$http.get('/api/users/id', {
+				params: {
+					name: user.name,
+					location: user.location
+				},
+				headers: {
+					'Access-Control-Allow-Origin': '*',
+					'ZUMO-API-VERSION': '2.0.0'
+				},
+				dataType: "json",
+				contentType: "application/json; charset=utf-8"
+			})
+			.then(function (res) {
+				$scope.retData = res;
+				var obj = $scope.retData;
+				if (obj.data.length > 0) //if there is, alert
+				{
+					alert('User exists!');
+				} else //if none, then POST
+				{
+				$http.post('/api/users', user, config)
+				.then(function (res) {
+					console.log(res.data);
+					alert('Added successfully!');
+					$scope.getNames();
+				})
+				.catch (function (res) {
+					alert(res);
+				});
+				}
+			})
+			.catch (function (res) {
+				alert(res);
+			});
+			
+		}
+	}
+
+	function delName(user) {
+		var confirmres = confirm("You are about to delete this record. Action cannot be undone. Continue?");
+		var retrievedId = "";
+
+		if (confirmres == true) {
+			//get the ID via web service
+			$http.get('/api/users/id', {
+				params: {
+					name: user.name,
+					location: user.location
+				},
+				headers: {
+					'Access-Control-Allow-Origin': '*',
+					'ZUMO-API-VERSION': '2.0.0'
+				},
+				dataType: "json",
+				contentType: "application/json; charset=utf-8"
+			})
+			.then(function (res) {
+				$scope.retData = res;
+				var obj = $scope.retData;
+				if (obj.data.length == 0) {
+					alert('No data found');
+				} else {
+					angular.forEach(obj.data, function (item) {
+						//perform delete after getting the ID and append it to url
+						$http.delete ('/api/users/' + item._id, config)
+						.then(function (res) {
+							$scope.getNames();
+							alert('item with item ID: ' + item._id + ' deleted');
+						});
+					});
+				}
+			})
+			.catch (function (res) {
+				alert(res);
+			});
+		}
+	}
+
+	$scope.addName = addName;
+	$scope.getNames = getNames;
+	$scope.delName = delName;
+
+})
---
+++ routes/api.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const router = express.Router(); //HTTP requests
+const MongoClient = require('mongodb').MongoClient; //MongoClient object.  No need to setup models for mongoose.
+const ObjectID = require('mongodb').ObjectID;
+
+// Connect
+const connection = (closure) => {
+    return MongoClient.connect('mongodb://joseph-test:joseph12345@ds042607.mlab.com:42607/testdb1', (err, db) => {
+        if (err) return console.log(err);
+
+        closure(db);
+    });
+};
+
+const sendError = (err, res) => {
+    response.status = 501;
+    response.message = typeof err == 'object' ? err.message : err;
+    res.status(501).json(response);
+};
+
+// Response handling
+let response = {
+    status: 200,
+    data: [],
+    message: null
+};
+
+
+//HTTP GET
+// Get users
+router.get('/users', (req, res) => {
+    connection((db) => {
+        db.collection('users')
+            .find()
+            .toArray()
+            .then((users) => {
+                //response.data = users;
+                res.json(users);
+            })
+            .catch((err) => {
+                sendError(err, res);
+            });
+    });
+});
+
+// Get user by Id
+router.get('/users/id', (req, res) => {
+    connection((db) => {
+        db.collection('users')
+            .find({"name": req.query.name, "location": req.query.location})
+            .toArray()
+            .then((users) => {
+                res.send(users);
+            })
+            .catch((err) => {
+                sendError(err, res);
+            });
+    });
+});
+
+//HTTP POST
+// Create users
+router.post('/users', (req, res) => {
+    connection((db) => {
+        db.collection('users')
+            .save({name: req.body.name, location: req.body.location}, {w: "majority", wtimeout: 20000})
+            .then((wresult) => {
+                res.send(wresult);
+            })
+            .catch((err) => {
+                sendError(err, res);
+            });
+    });
+    console.log(req.body + 'successfully added to db');
+});
+
+// Delete users
+router.delete('/users/:_id', (req, res) => {
+    console.log(req.params._id);
+    var id = JSON.stringify(req.params);
+    var query = {_id: "ObjectID("+req.params._id+")"}
+    var mongodb = require('mongodb');
+    console.log(query);
+    connection((db) => {
+        db.collection('users')
+            .remove({_id: new mongodb.ObjectID(req.params._id)})
+            .then((wresult) => {
+                res.send(wresult);
+            })
+            .catch((err) => {
+                sendError(err, res);
+            });
+    });
+});
+
+module.exports = router;
+
