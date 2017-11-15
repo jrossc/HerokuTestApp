3eac02584dd090f95165691ce24a63c334566317
JosephRossCajulis <joseph2coc@gmail.com>
2017-11-15 19:19:28 +0800

    Updated mongoose version and applied mongoose codes in api.js

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const router = express.Router(); //HTTP requests
-const MongoClient = require('mongodb').MongoClient; //MongoClient object.  No need to setup models for mongoose.
+//const MongoClient = require('mongodb').MongoClient; //MongoClient object.  No need to setup models for mongoose.
 const ObjectID = require('mongodb').ObjectID;
 
-// Connect
-const connection = (closure) => {
-    return MongoClient.connect(process.env.DATABASE_CREDENTIALS, (err, db) => {
-        if (err) return console.log(err);
+var mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_CREDENTIALS, {
+    useMongoClient: true
+});
 
-        closure(db);
-    });
-};
+var Users = require('./models/user');
+
+// Connect
+// const connection = (closure) => {
+//     return MongoClient.connect(process.env.DATABASE_CREDENTIALS, (err, db) => {
+//         if (err) return console.log(err);
+
+//         closure(db);
+//     });
+// };
 
 const sendError = (err, res) => {
     response.status = 501;
@@ -29,67 +36,103 @@ let response = {
 //HTTP GET
 // Get users
 router.get('/users', (req, res) => {
-    connection((db) => {
-        db.collection('users')
-            .find()
-            .toArray()
-            .then((users) => {
-                //response.data = users;
-                res.json(users);
-            })
-            .catch((err) => {
-                sendError(err, res);
-            });
+    // connection((db) => {
+    //     db.collection('users')
+    //         .find()
+    //         .toArray()
+    //         .then((users) => {
+    //             //response.data = users;
+    //             res.json(users);
+    //         })
+    //         .catch((err) => {
+    //             sendError(err, res);
+    //         });
+    // });
+    Users.find(function(err, users) {
+        if (err)
+            sendError(err);
+
+
+        res.json(users);
     });
 });
 
 // Get user by Id
 router.get('/users/id', (req, res) => {
-    connection((db) => {
-        db.collection('users')
-            .find({"name": req.query.name, "location": req.query.location})
-            .toArray()
-            .then((users) => {
-                res.send(users);
-            })
-            .catch((err) => {
-                sendError(err, res);
-            });
+    // connection((db) => {
+    //     db.collection('users')
+    //         .find({"name": req.query.name, "location": req.query.location})
+    //         .toArray()
+    //         .then((users) => {
+    //             res.send(users);
+    //         })
+    //         .catch((err) => {
+    //             sendError(err, res);
+    //         });
+    // });
+    Users.find({
+        "name": req.query.name,
+        "location": req.query.location
+    }, function(err, users) {
+        if (err)
+            sendError(err);
+
+        res.send(users);
     });
 });
 
 //HTTP POST
 // Create users
 router.post('/users', (req, res) => {
-    connection((db) => {
-        db.collection('users')
-            .save({name: req.body.name, location: req.body.location}, {w: "majority", wtimeout: 20000})
-            .then((wresult) => {
-                res.send(wresult);
-            })
-            .catch((err) => {
-                sendError(err, res);
-            });
+    // connection((db) => {
+    //     db.collection('users')
+    //         .save({name: req.body.name, location: req.body.location}, {w: "majority", wtimeout: 20000})
+    //         .then((wresult) => {
+    //             res.send(wresult);
+    //         })
+    //         .catch((err) => {
+    //             sendError(err, res);
+    //         });
+    // });
+    // console.log(req.body + 'successfully added to db');
+
+    Users.create({
+        name: req.body.name,
+        location: req.body.location
+    }, function(err, user) {
+        if (err)
+            res.send(err);
+
+        res.json(user);
     });
-    console.log(req.body + 'successfully added to db');
 });
 
 // Delete users
 router.delete('/users/:_id', (req, res) => {
-    console.log(req.params._id);
-    var id = JSON.stringify(req.params);
-    var query = {_id: "ObjectID("+req.params._id+")"}
-    var mongodb = require('mongodb');
-    console.log(query);
-    connection((db) => {
-        db.collection('users')
-            .remove({_id: new mongodb.ObjectID(req.params._id)})
-            .then((wresult) => {
-                res.send(wresult);
-            })
-            .catch((err) => {
-                sendError(err, res);
-            });
+    // console.log(req.params._id);
+    // var id = JSON.stringify(req.params);
+    // var query = {_id: "ObjectID("+req.params._id+")"}
+    // var mongodb = require('mongodb');
+    // console.log(query);
+    // connection((db) => {
+    //     db.collection('users')
+    //         .remove({_id: new mongodb.ObjectID(req.params._id)})
+    //         .then((wresult) => {
+    //             res.send(wresult);
+    //         })
+    //         .catch((err) => {
+    //             sendError(err, res);
+    //         });
+    // });
+    Users.remove({
+        _id: req.params._id
+    }, function(err, todo) {
+        if (err)
+            res.send(err);
+
+        res.json({
+            "status": "deleted"
+        });
     });
 });
 
--- server.js
+++ server.js
@@ -14,23 +14,20 @@
     const api = require('./routes/api'); // folder where routes file are stored
 
     // configuration =================
-    app.use(express.static(path.join(__dirname, 'public')));                 // set the static files location /public/img will be /img for users
-    
-
+    app.use(express.static(path.join(__dirname, 'public')));         // set the static files location /public/img will be /img for users
     app.use(morgan('dev'));                                         // log every request to the console
     app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
     app.use(bodyParser.json());                                     // parse application/json
     app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
     app.use(methodOverride());
 
-    //API location, must be placed after user of bodyparser
-    app.use('/api', api)                                            // outes
+    //API location, must be placed after use of bodyparser
+    app.use('/api', api)                                            // routes
         
     //log all other requests here
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'public/index.html'));
     });
-    
     
     // listen (start app with node server.js) ======================================
     const port = process.env.PORT || '3000';
