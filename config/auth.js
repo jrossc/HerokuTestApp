91a8870a82a6e84d31bd13f91c4bd1b4dbd68142
JosephRossCajulis <joseph2coc@gmail.com>
2017-11-30 01:24:29 +0800

    changed callbackURL to process.env

--- config/auth.js
+++ config/auth.js
@@ -6,7 +6,7 @@ module.exports = {
         'facebookAuth' : {
             'clientID'      :  process.env.FACEBOOK_ID, // your App ID
             'clientSecret'  :  process.env.FACEBOOK_APPSECRET, // your App Secret
-            'callbackURL'   : 'http://localhost:5000/auth/facebook/callback' || process.env.HOSTNAME + '/auth/facebook/callback',
+            'callbackURL'   :  process.env.HOSTNAME + '/auth/facebook/callback',
             'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
             'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
         },
