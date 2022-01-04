c285435cc080a6f556bbf660da227c657169240a
jrossc <joseph2coc@gmail.com>
2022-01-05 07:47:22 +0800

    hardcoded db url for test

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
     
-    'url' : process.env.DATABASE_CREDENTIALS // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    //'url': 'mongodb+srv://small4wardtest1:Jcajulis1!@cluster0.qke6u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    //'url' : process.env.DATABASE_CREDENTIALS // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url': 'mongodb+srv://small4wardtest1:Jcajulis1!@cluster0.qke6u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
     };
